55fca956dc5dc07c3b551919d52c1840926ee064
Vladyslav Bublii <53186169+VladyslavBublii@users.noreply.github.com>
2021-04-17 19:22:52 +0300

    Add-models
    First commit for task2

---
+++ Models(Task2)/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public IEnumerable<Guid> ProductsId { get; set; }
+    }
+}
---
+++ Models(Task2)/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Core.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string City { get; set; }
+
+        public string PostIndex { get; set; }
+
+        public User User { set; get; }
+    }
+}
